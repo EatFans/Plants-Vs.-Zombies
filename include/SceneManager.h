8074f909792f77a277773482982dd3b59d9e5038
EatFan <eatfan0921@163.com>
2023-11-12 00:23:44 +0800

    添加场景管理类模块

---
+++ include/SceneManager.h
@@ -0,0 +1,13 @@
+//
+// Created by EatFan on 2023/11/12.
+//
+
+#ifndef PLANTS_VS__ZOMBIES_SCENEMANAGER_H
+#define PLANTS_VS__ZOMBIES_SCENEMANAGER_H
+
+class SceneManager {
+public:
+private:
+};
+
+#endif //PLANTS_VS__ZOMBIES_SCENEMANAGER_H
